37d6e23e3cdef09a7b3fcb0e0200e3d0bbebcb4c
Hetarth Shah <hetarth02@gmail.com>
2024-09-15 18:21:29 +0530

    - updated deps and configs

--- source/svelte.config.js
+++ source/svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { sveltePreprocess } from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +8,7 @@ const config = {
     // for more information about preprocessors
     preprocess: [
         vitePreprocess(),
-        preprocess({
+        sveltePreprocess({
             postcss: true
         })
     ],
--- source/tailwind.config.ts
+++ source/tailwind.config.ts
@@ -1,7 +1,5 @@
 import { join } from 'path';
-import forms from '@tailwindcss/forms';
 import type { Config } from 'tailwindcss';
-import typography from '@tailwindcss/typography';
 import { skeleton } from '@skeletonlabs/tw-plugin';
 
 export default {
@@ -14,8 +12,6 @@ export default {
         extend: {}
     },
     plugins: [
-        typography,
-        forms,
         skeleton({
             themes: {
                 preset: [
